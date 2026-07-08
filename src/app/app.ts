58ef47991377686ba43e3526224705fa674bdbc0
albert-dev <al96@hotmail.es>
2026-07-08 10:20:41 +0200

    feat: "created checkbox component to use in card component"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Card } from './components/card/card';
+import { Checkbox } from './components/checkbox/checkbox';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Card],
+  imports: [RouterOutlet, Card, Checkbox],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
