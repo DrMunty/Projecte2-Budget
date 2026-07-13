6c23793b2233e4d7dcadb411dcef2127eaba6d0d
albert-dev <al96@hotmail.es>
2026-07-13 10:43:32 +0200

    test: "tested that when the checkbox is checked it displays the additional options of number of pages and languages"

--- src/app/components/card/card.spec.ts
+++ src/app/components/card/card.spec.ts
@@ -20,5 +20,15 @@ describe('Card', () => {
     expect(component).toBeTruthy();
   });
 
-  
+  it ('should deploy additional options such as number of languages and pages when the checkbox is checked', () => {
+    let checkBoxElement: HTMLInputElement = fixture.nativeElement.querySelector('input[type="checkbox"]');
+     expect(checkBoxElement.checked).toBe(false);
+     checkBoxElement.click();
+     fixture.detectChanges();
+     expect(checkBoxElement.checked).toBe(true);
+     const cardText: string = fixture.nativeElement.textContent;
+     expect(cardText).toContain('Nombre de pàgines');
+     expect(cardText).toContain('Nombre de llenguatges');
+  });
+
 });
