de9d388929ddbf74a507abe3bba217a3af81ef20
albert-dev <al96@hotmail.es>
2026-07-14 10:36:04 +0200

    feat: "created an array of data for the cards on dashboard.ts"

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Card } from '../card/card';
 import { signal } from '@angular/core';
+import type { CardData } from '../../models/cardData';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +11,24 @@ import { signal } from '@angular/core';
 })
 export class Dashboard {
 myDashboard = signal<CardData[]>([
-  {}
+  {
+    title: "Seo",
+    description: "Progamació d'una web responsive completa",
+    price: 300
+  },
 
-])
+    {
+    title: "Ads",
+    description: "Progamació d'una web responsive completa",
+    price: 400
+  },
+
+  {
+    title: "Web",
+    description: "Progamació d'una web responsive completa",
+    price: 500
+  }
+
+]);
+
 }
