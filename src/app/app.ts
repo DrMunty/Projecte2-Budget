3dfa08ad2569e330fc40c9e86f3d596521376558
albert-dev <al96@hotmail.es>
2026-07-15 10:58:12 +0200

    feat: "imported app-user-form into app"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { Dashboard } from './components/dashboard/dashboard';
-import { BudgetSummary } from './components/budget-summary/budget-summary';
+import { UserForm } from './components/user-form/user-form';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Dashboard],
+  imports: [Header, Dashboard, UserForm],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
